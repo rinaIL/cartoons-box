97e97a4419777dbefa9ad2e8ea1b94aef9349d23
Rina Solomon <rinagrom@gmail.com>
2020-11-03 23:43:13 +0200

    receiving all data

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,38 +2,118 @@ import React from 'react';
 import './Content.css';
 import {getItems, getVideo} from '../logic/youtube';
 import db from '../logic/database';
+import moment from 'moment';
+
+
+
+//it's going to be in the admin page
+////////////////////////////////////////////////////////
+//const keywords = ["Blippi", "ComeOutsideTV", "Get Well Soon Dr.Ranj teaches"]
+const keywords = ["Blippi", "ComeOutsideTV"]
+
+for (let k of keywords) {
+    db.removeData('keywords', key => key === k);
+}
+for (let k of keywords) {
+    db.addValue('keywords', k);
+}
+//////////////////////////////////////////////////////////////////////
 
 
 class Content extends React.Component {
     constructor(props){
         super(props);
-        this.state = { itemInCardList:null}
+        this.state = { videos:[],videosWithDuration:[]};
     }
 
     async componentDidMount() {
-        const items = await getItems();   
-        const videosDuration = await getVideo();
-        const itemInCardList = items.map(video => <img className='ui image' src={video.snippet.thumbnails.medium.url} alt={video.snippet.description}/> );     
-        this.setState({itemInCardList});
+        
 
-        /// for test only 
-        for (let video of items) {
-            db.addValue('videos', video);
+        const data = []
+
+        for (let k of db.getCollection('keywords')) {
+            const items = await getItems(k); 
+            const obj = {
+                term:k,
+                data:items
+            }
+            data.push(obj)
         }
-        for (let video of videosDuration) {
-            db.addValue('videoWithDuration', video.contentDetails.duration);
-        }    
+
+        const videosWithDuration = []
+
+        await Promise.all(data.map(async (videosPerKeyword)=> {
+            Promise.all(videosPerKeyword.data.map(async (video) => {
+                if (typeof(video.id.videoId) !=="undefined"){
+                    const videoDetails = await getVideo(video.id.videoId);
+                    Promise.all(videoDetails.map(async (details) => {                       
+                        const duration = parseInt(moment.duration(details.contentDetails.duration).asMinutes(),10);
+                        const durationHour = parseInt(moment.duration(details.contentDetails.duration).asHours(),10);                        
+                        if (duration !== 0 && durationHour === 0) {
+                            const obj = {
+                                id:video.id.videoId,
+                                duration: duration
+                            }
+                            videosWithDuration.push(obj);
+                        }
+
+                    }))
+                }
+            }))
+
+        }))
+
+        
+        this.setState({videos:data});
+        this.setState({videosWithDuration:videosWithDuration});
+      
+        console.log("videosWithDuration", this.state.videosWithDuration);
 
     }
+
+    getVideosCart(key) {
+        console.log("state videos", this.state.videos);
+
+        const filteredVideo =  this.state.videos.filter((obj) => obj.term === key);
+        console.log("DATA", filteredVideo);
+
+            
+        const videoCard =  filteredVideo[0].data.map((v) => {
+            console.log("VIDEO", v);
+            const obj = {
+                title:v.snippet.title,
+                picture:v.snippet.thumbnails.medium.url
+            }
+            return <VideoCard key={v.id.videoId} video={obj} />
+        })
+
+        return videoCard;
+    }
+       
  
     render() {
           return (          
               <div>
-                <div className="videos">{this.state.itemInCardList}</div>
+                {db.getCollection('keywords').map((k) => {
+                    return <div key={k}><h3>{k} {this.state.videos.lenght > 0 ? this.getVideosCart(k):''}</h3></div>
+                })}
               </div>          
           );
     }
 
+}
+
+class VideoCard extends React.Component {
+    render() {
+        const video = this.props.video;
+        return (
+            <article>
+                <h4>{video.title}</h4>
+                <div><img src={video.picture}/></div>
+                <p>Click her for more</p>
+            </article>
+        )
+    }
 }
 
 export default Content;
--- src/logic/youtube.js
+++ src/logic/youtube.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
-const KEY = 'AIzaSyDB95K_UPZjepFNtQ6wqqGPtgL7m1LDMvc';
+const KEY = 'AIzaSyCI1d35GUFkFt8Y5NDXRAwaEdVlfwo-38Q';
 const basicQueary = 'https://www.googleapis.com/youtube/v3/search';
 const videoQuery = 'https://www.googleapis.com/youtube/v3/videos';
 
 
-async function getResults() {
+async function getResults(query) {
     const result = await axios.get(basicQueary,{params: {
         part: 'snippet',
-        maxResults: 10,
+        maxResults: 3,
         key: KEY,
-        q: "ComeOutsideTV"
+        q: query
     }});
     return result.data.items;
 }
 
-async function get() {
+async function get(id) {
     const result = await axios.get(videoQuery,{params: {
-        id: 'Z9YTB5XkQdg',
+        id: id,
         part: 'contentDetails',
         key: KEY,
         
@@ -24,11 +24,11 @@ async function get() {
     return result.data.items;
 }
 
-export async function getItems() {
-    return await getResults();
+export async function getItems(query) {
+    return await getResults(query);
 }
 
-export async function getVideo() {
-    return await get();
+export async function getVideo(id) {
+    return await get(id);
 }
 
